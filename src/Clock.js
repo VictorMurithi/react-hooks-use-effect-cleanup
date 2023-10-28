e03812d3ec2eaa04a623cba28687f3c74b2021cc
victor <victor.murithi@student.moringaschool.com>
2023-10-28 11:00:25 +0300

    completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,12 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timer1D = setInterval(() => {
       setTime(new Date());
     }, 1000);
+    return function cleanup(){
+      clearInterval(timer1D);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
